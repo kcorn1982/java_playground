c556e9b7f54c4aaf81290c4b7f017075a0dcb667
Kieran Cornwall <kcorn1982@gmail.com>
2021-12-06 16:34:19 +0000

    noooooo

--- src/test/java/challenges/week_1/fizzbuzz/FizzBuzzTests.java
+++ src/test/java/challenges/week_1/fizzbuzz/FizzBuzzTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class FizzBuzzTests {
-    private final String[] fizzBuzzTestDataArray = {"1", "2", "Fizz", "4", "Buzz", "5", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+    private final String[] fizzBuzzTestDataArray = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
     private static FizzBuzz fizzBuzz = new FizzBuzz();
     private final int fizzBuzzTestRange = 15;
 
